46f9d1bfadb15c39f48de9280c17a90f56fb078f
Alisa <alisabox@mail.ru>
2021-07-30 10:28:15 +0900

    Добавляет модули view

---
+++ src/view/loading.js
@@ -0,0 +1,5 @@
+const createLoading = () => (
+  '<h2 class="films-list__title">Loading...</h2>'
+);
+
+export {createLoading};
---
+++ src/view/show-button.js
@@ -0,0 +1,5 @@
+const createShowButton = () => (
+  '<button class="films-list__show-more">Show more</button>'
+);
+
+export {createShowButton};
